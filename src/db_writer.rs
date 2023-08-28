b380a28a62db9ba3219f00f0724f04e7a8610734
Adam Tekle <59581858+Unexpected-Error@users.noreply.github.com>
2023-08-27 17:24:36 -0700

    progressss

--- src/db_writer.rs
+++ src/db_writer.rs
@@ -1,10 +1,8 @@
 pub(crate) mod types {
-    use std::ops::{Deref, DerefMut};
     // Error Handling
     use anyhow::{anyhow, Result};
     // Import Special Types
     use ipnetwork::IpNetwork;
-    use rayon::prelude::*;
     // Serde Derives
     use serde::{Deserialize, Serialize};
     use sqlx::postgres::{PgHasArrayType, PgTypeInfo};
@@ -31,7 +29,7 @@ pub(crate) mod types {
                     true => None,
                 },
                 prefix: self.prefix,
-                as_path_segments: segar::from(match self.announcing {
+                as_path_segments: match self.announcing {
                     true => self
                         .as_path_segments
                         .ok_or(anyhow!("NO AS-PATH WHEN ANNOUNCING"))?,
@@ -41,7 +39,7 @@ pub(crate) mod types {
                         }
                         Some(n) => n,
                     },
-                }),
+                },
             })
         }
     }
@@ -52,14 +50,22 @@ pub(crate) mod types {
         pub(crate) asn: i64,
         pub(crate) announcements: Vec<Announcement>,
     }
+
     #[derive(sqlx::Type, sqlx::FromRow, Debug, Serialize, Deserialize, Clone)]
     #[sqlx(type_name = "announcement")]
     pub struct Announcement {
         pub(crate) start_time: Option<f64>,
         pub(crate) stop_time: Option<f64>,
         pub(crate) prefix: IpNetwork,
-        pub(crate) as_path_segments: segar,
+        pub(crate) as_path_segments: Vec<ASPathSeg>,
     }
+
+    impl PgHasArrayType for Announcement {
+        fn array_type_info() -> PgTypeInfo {
+            PgTypeInfo::with_name("_announcement")
+        }
+    }
+
     #[derive(sqlx::Type, Debug, Serialize, Deserialize, Clone)]
     #[sqlx(type_name = "as_path_segment")]
     pub struct ASPathSeg {
@@ -74,56 +80,55 @@ pub(crate) mod types {
         }
     }
 
-    #[derive(sqlx::Type, Debug, Serialize, Deserialize, Clone)]
-    #[sqlx(type_name = "as_path_w")]
-    pub struct segar {
-        a_p: Vec<ASPathSeg>,
-    }
-
-    impl PgHasArrayType for segar {
-        fn array_type_info() -> PgTypeInfo {
-            PgTypeInfo::with_name("_as_path_seg_w")
-        }
-    }
-
-    impl Deref for segar {
-        type Target = Vec<ASPathSeg>;
-
-        fn deref(&self) -> &Self::Target {
-            &self.a_p
-        }
-    }
-
-    impl DerefMut for segar {
-        fn deref_mut(self: &mut segar) -> &mut Vec<ASPathSeg> {
-            &mut self.a_p
-        }
-    }
-
-    impl From<Vec<ASPathSeg>> for segar {
-        fn from(value: Vec<ASPathSeg>) -> Self {
-            Self { a_p: value }
-        }
-    }
+    // #[derive(sqlx::Type, Debug, Serialize, Deserialize, Clone)]
+    // #[sqlx(type_name = "as_path_w")]
+    // pub struct segar {
+    //     a_p: Vec<ASPathSeg>,
+    // }
+    //
+    // impl PgHasArrayType for segar {
+    //     fn array_type_info() -> PgTypeInfo {
+    //         PgTypeInfo::with_name("_as_path_seg_w")
+    //     }
+    // }
+    //
+    // impl Deref for segar {
+    //     type Target = Vec<ASPathSeg>;
+    //
+    //     fn deref(&self) -> &Self::Target {
+    //         &self.a_p
+    //     }
+    // }
+    //
+    // impl DerefMut for segar {
+    //     fn deref_mut(self: &mut segar) -> &mut Vec<ASPathSeg> {
+    //         &mut self.a_p
+    //     }
+    // }
+    //
+    // impl From<Vec<ASPathSeg>> for segar {
+    //     fn from(value: Vec<ASPathSeg>) -> Self {
+    //         Self { a_p: value }
+    //     }
+    // }
 }
 
-use crate::db_writer::types::{ASPathSeg, Announcement, RawAnnouncement, R_ASN};
-use anyhow::Context;
-use log::{info, warn};
-// use rayon::prelude::*;
-// use sqlx::{Row};
+//C reate --> Make new
+//R ead   --> Collect
+//U date  --> Change
+//D elete --> Remove
+// -------------------
+//Insert  --> Autopick between C and U
+
+// types
 use ipnetwork::IpNetwork;
-use std::error::Error;
 use std::net::IpAddr;
-use types::Orange;
-
-const PG_URL: &str = "postgres://postgres:postgrespw@localhost:55000/BGP";
-const INSERT: &str = "INSERT INTO Orange (asn, announcements) VALUES ($1, $2)";
+use types::{ASPathSeg, Announcement, Orange, RawAnnouncement, R_ASN};
+// errors && logs
+use anyhow::{anyhow, Context};
+use log::{info, warn};
+const PG_URL: &str = "postgres://postgres:postgrespw@localhost:55000/BGP2";
 const DELETE_ALL: &str = "TRUNCATE Orange";
-
-// const OLD_IP_CHECK: &str = "SELECT asn, prefixes FROM orange WHERE EXISTS (SELECT 1 FROM unnest(prefixes) AS p WHERE $1 << p)";
-const IP_CHECK: &str =
-    "SELECT * FROM Orange WHERE EXISTS ( SELECT 1 FROM unnest(announcements) AS a WHERE (a.prefix >> $1));";
 
 pub(crate) async fn open_db() -> Result<sqlx::PgPool, anyhow::Error> {
     // TODO: Add user/password args and pull from .env
@@ -140,17 +145,15 @@ pub(crate) async fn open_db() -> Result<sqlx::PgPool, anyhow::Error> {
     Ok(pool)
 }
 
-async fn insert_new_ann(
+async fn create_announcement(
     ann: Announcement,
     ASN: i64,
     pool: &sqlx::PgPool,
 ) -> Result<(), anyhow::Error> {
     sqlx::query!(
         "UPDATE Orange SET announcements = array_append(announcements, $2) WHERE asn = $1",
-        //try announcements ann as () to use _ as _ on inner
-        //        ann as _,
         ASN,
-        ann as _
+        ann as Announcement
     )
     .execute(pool)
     .await
@@ -158,10 +161,36 @@ async fn insert_new_ann(
     Ok(())
 }
 
-pub(crate) async fn insert(
-    ann: (RawAnnouncement, R_ASN),
+async fn update_announcement(
+    ann_to_update: Announcement,
+    new_ann: Announcement,
+    ASN: i64,
     pool: &sqlx::PgPool,
 ) -> Result<(), anyhow::Error> {
+    sqlx::query!(
+        "UPDATE Orange SET announcements = array_replace(announcements, $1, $2) WHERE asn = $3",
+        ann_to_update as Announcement,
+        new_ann as Announcement,
+        ASN as i64
+    )
+    .execute(pool)
+    .await?;
+    Ok(())
+}
+
+pub(crate) async fn create_orange(orange: Orange, pool: &sqlx::PgPool) -> Result<(), anyhow::Error> {
+    sqlx::query!(
+        r#"INSERT INTO Orange (asn, announcements) VALUES ($1, $2)"#,
+        orange.asn,
+        orange.announcements as Vec<Announcement>
+    )
+    .execute(pool)
+    .await
+    .context("here")?;
+    Ok(())
+}
+
+pub(crate) async fn insert_announcement(ann: (RawAnnouncement, R_ASN),pool: &sqlx::PgPool) -> Result<(), anyhow::Error> {
     let ASN = ann.1 as i64;
     let ann = ann.0;
 
@@ -171,7 +200,27 @@ pub(crate) async fn insert(
                 "Inserting new announcement, prefix: {}, peer ASN: {}",
                 ann.prefix, ASN
             );
-            insert_new_ann(ann.to_announcement_as_new()?, ASN, pool).await?;
+            match sqlx::query!(
+                r#"select exists(select 1 from orange where asn=$1) AS "exists""#,
+                ASN
+            )
+            .fetch_one(pool)
+            .await?
+            .exists
+            .ok_or(anyhow!("Didn't find exists bool"))?
+            {
+                true => create_announcement(ann.to_announcement_as_new()?, ASN, pool).await?,
+                false => {
+                    create_orange(
+                        Orange {
+                            asn: ASN,
+                            announcements: vec![ann.to_announcement_as_new()?],
+                        },
+                        pool,
+                    )
+                    .await?
+                }
+            }
         }
         false => {
             info!(
@@ -179,7 +228,7 @@ pub(crate) async fn insert(
                 ann.prefix, ASN
             );
 
-            let latest_start_ann: Option<Announcement> = sqlx::query_as!( 
+            match sqlx::query_as!(
                 Announcement,
                 r#"
                 SELECT a.start_time, a.stop_time, a.prefix as "prefix!: IpNetwork", a.as_path_segments as "as_path_segments!: Vec<ASPathSeg>"
@@ -196,36 +245,45 @@ pub(crate) async fn insert(
                 ann.prefix
             )
                 .fetch_optional(pool)
-                .await?;
-
-            if let Some(start_ann) = latest_start_ann {
-                info!("\tFOUND, updating stop time for {:?}", start_ann);
-                // An existing announcement with a matching prefix was found, update stop_time
-                let mut new_ann = start_ann.clone();
-                new_ann.stop_time = Some(ann.time); // Set your desired start_time
-                sqlx::query!(
-                    "UPDATE Orange SET announcements = array_replace(announcements, $1, $2) WHERE asn = $3",
-                    start_ann as Announcement,
-                    new_ann as Announcement,
-                    ASN as i64)
-                    .execute(pool)
-                    .await?;
-            } else {
-                let n = ann.to_announcement_as_new()?;
-                info!("No ann found, inserting as naked withdraw: {:?}", n);
-                insert_new_ann(n, ASN, pool).await?;
+                .await? {
+                None => {
+                    let n = ann.to_announcement_as_new()?;
+                    info!("No ann found, inserting as naked withdraw: {:?}", n);
+                    match sqlx::query!(
+                        r#"select exists(select 1 from orange where asn=$1) AS "exists""#,
+                        ASN
+                    )
+                        .fetch_one(pool)
+                        .await?
+                        .exists
+                        .ok_or(anyhow!("Didn't find exists bool"))?
+                    {
+                        true => {create_announcement(n, ASN, pool).await?}
+                        false => {create_orange(Orange { asn: ASN, announcements: vec![n] }, pool).await?}
+                    }
+                }
+                Some(start_ann) => {
+                    info!("\tFOUND, updating stop time for {:?}", start_ann);
+                    // An existing announcement with a matching prefix was found, update stop_time
+                    let mut new_ann = start_ann.clone();
+                    new_ann.stop_time = Some(ann.time); // Set your desired start_time
+                    update_announcement(start_ann, new_ann, ASN, pool).await?;
+                }
             }
         }
     }
-    // let res = sqlx::query(INSERT)
-    //     .bind(&orange.ASN)
-    //     .bind(&orange.prefixes)
-    //     .execute(pool)
-    //     .await?;
+    
     Ok(())
 }
 
-pub(crate) async fn delete(pool: &sqlx::PgPool) -> Result<(), sqlx::Error> {
+/// Given a PG database Pool [`sqlx::PgPool`], drops all AS (Oranges)
+/// WARNING: GIVES NO WARNING BEFORE DELETING ALL DATA
+///
+/// # Examples
+/// ```
+/// delete_all(&pool).await? // Bye bye data
+/// `
+pub(crate) async fn delete_all(pool: &sqlx::PgPool) -> Result<(), sqlx::Error> {
     warn!("Truncating Orange Table");
     sqlx::query(DELETE_ALL).execute(pool).await?;
     Ok(())
@@ -236,70 +294,31 @@ pub(crate) async fn delete(pool: &sqlx::PgPool) -> Result<(), sqlx::Error> {
 /// ```
 /// search("1.0.0.1".parse()?, &pool).await? // Does any AS declare 1.0.0.1? Probably, Cloudflare owns 1.0.0.0/24
 /// ```
-pub(crate) async fn search(ip: IpAddr, pool: &sqlx::PgPool) -> Result<Option<Orange>, sqlx::Error> {
-    todo!()
-    // warn!("Truncating Orange Table");
-    // let res = sqlx::query(IP_CHECK)
-    //     .bind(ip)
-    //     .fetch_optional(pool)
-    //     .await?;
-    // match res {
-    //     None => { Ok(None) }
-    //     Some(x) => {
-    //         Ok(
-    //             Some(
-    //                 Orange {
-    //                     ASN: x.get("id"),
-    //                     prefixes: x.get("prefixes"),
-    //                 }
-    //             )
-    //         )
-    //     }
-    // }
+pub(crate) async fn ip_search(ip: IpAddr,pool: &sqlx::PgPool) -> Result<Option<Orange>, sqlx::Error> {
+    let res = sqlx::query_as!(
+        Orange,
+        r#"SELECT asn, announcements as "announcements: Vec<Announcement>" FROM Orange WHERE EXISTS ( SELECT 1 FROM unnest(announcements) AS a WHERE (a.prefix >> $1));"#,
+        IpNetwork::from(ip)
+    )
+        .fetch_optional(pool)
+        .await?;
+    Ok(res)
 }
 
-pub(crate) async fn getall(ip: IpAddr, pool: &sqlx::PgPool) -> Result<(), sqlx::Error> {
-    type tbl = Vec<Orange>;
+/// Given a PG database Pool [`sqlx::PgPool`], finds if all AS (Orange)
+///
+/// WARNING: Loads huge amounts of data into memory
+/// # Examples
+/// ```
+/// let allOranges: Vec<Orange> = getall(&pool).await? // Collect all Oranges
+/// ```
+pub(crate) async fn read_all(pool: &sqlx::PgPool) -> Result<Vec<Orange>, sqlx::Error> {
+    warn!("Loading all Oranges into memory...");
     let latest_start_ann = sqlx::query_as!(
         Orange,
-        r#"select asn, announcements as "announcements: Vec<Announcement>" from Orange"# // r#"
-                                                                                         // SELECT a.start_time, a.stop_time, a.prefix, a.as_path, a.as_path_is_seq
-                                                                                         // FROM Orange AS o
-                                                                                         // CROSS JOIN LATERAL (
-                                                                                         //     SELECT a.start_time, a.stop_time, a.prefix, a.as_path, a.as_path_is_seq
-                                                                                         //     FROM unnest(o.announcements) AS a
-                                                                                         //     WHERE o.asn = $1 AND a.prefix = $2
-                                                                                         //     ORDER BY a.start_time DESC
-                                                                                         //     LIMIT 1
-                                                                                         // ) AS a
-                                                                                         // "#,
-                                                                                         // ASN,
-                                                                                         // ann.prefix
+        r#"select asn, announcements as "announcements: Vec<Announcement>" from Orange"#
     )
     .fetch_all(pool)
     .await?;
-    Ok(())
-}
-
-pub(crate) async fn getasll(ip: IpAddr, pool: &sqlx::PgPool) -> Result<(), sqlx::Error> {
-    type tbl = Vec<Orange>;
-    let latest_start_ann = sqlx::query_as!(
-        Orange,
-        r#"select asn, announcements as "announcements: Vec<Announcement>" from Orange"# // r#"
-                                                                                         // SELECT a.start_time, a.stop_time, a.prefix, a.as_path, a.as_path_is_seq
-                                                                                         // FROM Orange AS o
-                                                                                         // CROSS JOIN LATERAL (
-                                                                                         //     SELECT a.start_time, a.stop_time, a.prefix, a.as_path, a.as_path_is_seq
-                                                                                         //     FROM unnest(o.announcements) AS a
-                                                                                         //     WHERE o.asn = $1 AND a.prefix = $2
-                                                                                         //     ORDER BY a.start_time DESC
-                                                                                         //     LIMIT 1
-                                                                                         // ) AS a
-                                                                                         // "#,
-                                                                                         // ASN,
-                                                                                         // ann.prefix
-    )
-    .fetch_all(pool)
-    .await?;
-    Ok(())
+    Ok(latest_start_ann)
 }
--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,15 @@ use db_writer::{open_db, types::Orange};
 // BGP
 mod bgp;
 
-use crate::db_writer::types::Announcement;
-use crate::db_writer::{insert, search};
+use crate::db_writer::{insert_announcement};
 use bgp::{collect_bgp, parse_bgp};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    
     env_logger::init();
     let pool = open_db().await?;
-    // info!("Querying...");
-    // delete(&pool).await?;
-    //
-    // // Store test ASN
-    // create(&Orange { ASN: 65000, prefixes: vec![] }, &pool).await?;
-
-    //insert_new_ann(/* db_writer::types::Announcement */, 65000, /* &Pool<Postgres> */).await?;
-
+    
     let data = tokio::task::spawn_blocking(move || {
         parse_bgp(collect_bgp(1692223200, 1692223953))
         // 20230816.2200
@@ -41,33 +34,19 @@ async fn main() -> Result<()> {
     .with_context(|| "Collecting BGP data panicked")??;
 
     for point in data {
-        insert(point, &pool).await?;
+        insert_announcement(point, &pool).await?;
     }
 
     Ok(())
 }
 
-// async fn insert_new_ann(
-//     ann: Announcement,
-//     ASN: i64,
-//     pool: &sqlx::PgPool,
-// ) -> Result<(), anyhow::Error> {
-//     sqlx::query!(
-//         "UPDATE Orange SET announcements = array_append(announcements, $2) WHERE asn = $1",
-//         //try announcements ann as () to use _ as _ on inner
-// //        ann as _,
-//         ASN,
-//         ann as _
-//     )
-//         .execute(pool)
-//         .await.context("here")?;
-//     Ok(())
-// }
 
 // let mut map = StringPatriciaMap::new();
 // map.insert("1100", "ASN6500");
 // map.insert("1110", "ASN6501");
 
 // TODO: Add parralelled insert using pool
+// 20MB network   | 190 MB disk || 15 minutes data | 290 minutes processing time 
+// 175 GB network | 1.7 TB      || 3 months data   | 4.8 years processing time 
 // TODO: Collect timing and size stats
 // TODO: Use docker to automate
